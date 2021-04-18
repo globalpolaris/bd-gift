0130da12bb80cbe78a26bc9af401d84c9b7151be
globalpolaris <54710635+globalpolaris@users.noreply.github.com>
2021-04-18 16:01:26 +0700

    finish homepage responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Header from './component/Header';
-import Timer from './component/Countdown'
-import SearchBar from './component/Search'
-import Night from './component/NightMode'
-import ReqDictionary from './component/Dictionary'
+import Header from "./component/Header";
+import Timer from "./component/Countdown";
+import Night from "./component/NightMode";
+import ReqDictionary from "./component/Dictionary";
 
 function App() {
   return (
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import App from '../App'
 
 const Header = () => {
+  const [toggle, setToggled] = useState(false);
   return (
       <header>
-          <h2 className="title">P R O J E C T - Z</h2>
-            <div className="options">
-                <ul className="nav">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">How it started</a></li>
-                    <li><a href="#"><span>Your Gift</span></a></li>
-                </ul>
-            </div>
+        <h2 className="title">P R O J E C T - Z</h2>
+        <div className="options">
+          <div className="hamburger">
+            <input
+              className="chck"
+              type="checkbox"
+              onChange={() => setToggled((toggle) => !toggle)}
+            />
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <ul className={toggle ? "nav slide" : "nav"}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#">How it started</a>
+            </li>
+            <li>
+              <a href="#">
+                <span>Your Gift</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </header>
-  )
+
+  );
 };
 
 export default Header;
