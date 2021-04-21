c91bede4d4ef290a590fd028f35afddbfc0934e8
globalpolaris <54710635+globalpolaris@users.noreply.github.com>
2021-04-21 17:05:24 +0700

    .

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
