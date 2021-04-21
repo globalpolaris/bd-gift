7327bd41b28de76927e560d8a290fb8f6baedb46
globalpolaris <54710635+globalpolaris@users.noreply.github.com>
2021-04-21 15:01:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./component/Header";
-import Timer from "./component/Countdown";
 import Night from "./component/NightMode";
-import ReqDictionary from "./component/Dictionary";
 import Home from "./component/Page/Home";
-import How from "./component/Page/How"
-import Gift from './component/Page/Gift'
+import How from "./component/Page/How";
+import Gift from "./component/Page/Gift";
 
 function App() {
   return (
